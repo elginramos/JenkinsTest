11be5f99e9ef99bbc1a8ccb52ac44fda471cc236
elginramos <elgin.ramos@gmail.com>
2024-05-01 14:35:37 +0800

    third commit

--- testCLI.m
+++ testCLI.m
@@ -1,7 +1,7 @@
 homeDir = pwd;
 cd(fullfile(pwd, 'Add2Numbers'));
-testData1 = [1, 2, 3, 4];
-testData2 = [5, 6, 7, 8];
+testData1 = [1, 2, 3, 4, 0];
+testData2 = [5, 6, 7, 8, 0];
 for i = 1:length(testData1)
     disp(['Test Case ', num2str(i), ': ', num2str(testData1(i)), ' + ', num2str(testData2(i))])
     result = Add2Numbers(testData1(i), testData2(i));
